fd5cfcf65e5b02ee9360ae975651d9c3e6c19003
ZarciaM <hei.zarcia@gmail.com>
2026-06-25 08:13:39 +0300

    fix: add centralized validateObjectId middleware, apply to BMI delete route

--- src/modules/bmi/bmi.routes.ts
+++ src/modules/bmi/bmi.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
 import { validate } from "../../middlewares/validate.middleware.js";
+import { validateObjectId } from "../../middlewares/validateObjectId.middleware.js";
 import { createBmiEntrySchema } from "./bmi.validation.js";
 import * as controller from "./bmi.controller.js";
 
@@ -9,6 +10,6 @@ const router = Router();
 router.post("/", authMiddleware, validate(createBmiEntrySchema), controller.create);
 router.get("/history", authMiddleware, controller.getHistory);
 router.get("/latest", authMiddleware, controller.getLatest);
-router.delete("/:id", authMiddleware, controller.remove);
+router.delete("/:id", authMiddleware, validateObjectId(), controller.remove);
 
 export default router;
